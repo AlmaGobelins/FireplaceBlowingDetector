ea25559708004a1f34a3361d1a1d05f9e41ab7b3
Mathieu <mathieu.dubart@edu.gobelins.fr>
2024-12-17 20:36:08 +0100

    add: More control over fireplace route and ip

--- FireplaceBlowingDetector/ContentView.swift
+++ FireplaceBlowingDetector/ContentView.swift
@@ -11,17 +11,27 @@ struct ContentView: View {
     @StateObject private var microphoneMonitor = MicrophoneMonitor()
     @ObservedObject var wsClient = WebSocketClient.shared
     @State var connectedToServer: Bool = false
-    
     @State var spheroIsConnected = false
     
+    @State private var ip: String = "192.168.0.132:8080"
+    @State private var route: String = "phoneFireplace"
     
-    //let spherosNames: [String] = ["SB-C7A8"]
-    let spherosNames: [String] = ["SB-42C1"]
-    //let spherosNames: [String] = ["SB-A729"]
+    let spherosNames: [String] = ["SB-42C1"] // "SB-C7A8" - "SB-A729"
+    
     private let collisionDetector = CollisionDetector(toyBox: SharedToyBox.instance)
     
     var body: some View {
         VStack {
+            HStack {
+                TextField("IP:", text: $ip)
+                TextField("Route:", text: $route)
+                
+                Button("Connect") {
+                    self.connectedToServer = wsClient.connectTo(route: route)
+                    microphoneMonitor.startMonitoring()
+                }
+            }
+            
             Text("Recording...")
                 .font(.title)
                 .foregroundStyle(.red)
@@ -33,9 +43,10 @@ struct ContentView: View {
             Text("Connecté au server : \(connectedToServer)")
         }
         .padding()
+        .onChange(of: ip) {
+            wsClient.ipAdress = ip
+        }
         .onAppear {
-            connectedToServer = wsClient.connectTo(route:"phoneFireplace")
-            microphoneMonitor.startMonitoring()
             SharedToyBox.instance.searchForBoltsNamed(spherosNames) { err in
                 if err == nil {
                     print("Connected to sphero")
@@ -53,7 +64,7 @@ struct ContentView: View {
         }
         .onChange(of: microphoneMonitor.isSouffling) {
             if self.connectedToServer && microphoneMonitor.isSouffling {
-                wsClient.sendMessage("souffle", toRoute: "phoneFireplace")
+                wsClient.sendMessage("souffle", toRoute: route)
             }
         }
     }
